a96ec62f01df59f35b97b2e86cc7f297df1a7844
nabil-k <duomaster01@gmail.com>
2022-08-23 16:52:55 -0700

    added getShadowTokens test

--- packages/theming/default-theme/src/__tests__/default-theme.test.ts
+++ packages/theming/default-theme/src/__tests__/default-theme.test.ts
@@ -1,7 +1,11 @@
 import lightAliasTokens from '@fluentui-react-native/design-tokens-windows/light/tokens-aliases.json';
 import darkAliasTokens from '@fluentui-react-native/design-tokens-windows/dark/tokens-aliases.json';
 import { hcAliasTokens } from '../highContrast/tokens-alias';
+import lightShadowTokens from '@fluentui-react-native/design-tokens-windows/light/tokens-shadow.json';
+import darkShadowTokens from '@fluentui-react-native/design-tokens-windows/dark/tokens-shadow.json';
+import hcShadowTokens from '@fluentui-react-native/design-tokens-win32/hc/tokens-shadow.json';
 import { getAliasTokens } from '../getTokens';
+import { getShadowTokens } from '../getTokens';
 
 const getAliasTokensTable = [
   ['light', lightAliasTokens],
@@ -9,6 +13,16 @@ const getAliasTokensTable = [
   ['highContrast', hcAliasTokens],
 ];
 
-it.concurrent.each(getAliasTokensTable)('getAliasTokens test', async (mode, tokens) => {
+it.concurrent.each(getAliasTokensTable)('getAliasTokens test', async (mode: any, tokens: any) => {
   expect(getAliasTokens(mode)).toBe(tokens);
+});
+
+const getShadowTokensTable = [
+  ['light', lightShadowTokens.shadow],
+  ['dark', darkShadowTokens.shadow],
+  ['highContrast', hcShadowTokens.shadow],
+];
+
+it.concurrent.each(getShadowTokensTable)('getShadowTokens test', async (mode: any, tokens: any) => {
+  expect(getShadowTokens(mode)).toBe(tokens);
 });
