4c48f2c292da6f6e90881c45e2253e07dd03ae44
nabil-k <duomaster01@gmail.com>
2022-08-23 18:16:29 -0700

    created createDefaultTheme test

--- packages/theming/default-theme/src/__tests__/default-theme.test.ts
+++ packages/theming/default-theme/src/__tests__/default-theme.test.ts
@@ -6,6 +6,20 @@ import darkShadowTokens from '@fluentui-react-native/design-tokens-windows/dark/
 import hcShadowTokens from '@fluentui-react-native/design-tokens-win32/hc/tokens-shadow.json';
 import { getAliasTokens } from '../getTokens';
 import { getShadowTokens } from '../getTokens';
+import { createDefaultTheme } from '../createDefaultTheme';
+import { defaultFluentDarkTheme, defaultFluentHighConstrastTheme, defaultFluentTheme } from '../defaultTheme';
+import { ThemeReference } from '@fluentui-react-native/theme';
+import { Theme } from '@fluentui-react-native/theme-types';
+
+const lightThemeRef = new ThemeReference({} as Theme, defaultFluentTheme);
+const darkThemeRef = new ThemeReference({} as Theme, defaultFluentDarkTheme);
+const highContrastThemeRef = new ThemeReference({} as Theme, defaultFluentHighConstrastTheme);
+
+const createDefaultThemeTable = [
+  [{ paletteName: 'TaskPane', appearance: 'light' }, lightThemeRef],
+  [{ paletteName: 'TaskPane', appearance: 'dark' }, darkThemeRef],
+  [{ paletteName: 'TaskPane', appearance: 'highContrast' }, highContrastThemeRef],
+];
 
 const getAliasTokensTable = [
   ['light', lightAliasTokens],
@@ -13,15 +27,19 @@ const getAliasTokensTable = [
   ['highContrast', hcAliasTokens],
 ];
 
-it.concurrent.each(getAliasTokensTable)('getAliasTokens test', async (mode: any, tokens: any) => {
-  expect(getAliasTokens(mode)).toBe(tokens);
-});
-
 const getShadowTokensTable = [
   ['light', lightShadowTokens.shadow],
   ['dark', darkShadowTokens.shadow],
   ['highContrast', hcShadowTokens.shadow],
 ];
+
+it.concurrent.each(createDefaultThemeTable)('createDefaultTheme test', async (mode: any, tokens: any) => {
+  expect(createDefaultTheme(mode)).toBe(tokens);
+});
+
+it.concurrent.each(getAliasTokensTable)('getAliasTokens test', async (mode: any, tokens: any) => {
+  expect(getAliasTokens(mode)).toBe(tokens);
+});
 
 it.concurrent.each(getShadowTokensTable)('getShadowTokens test', async (mode: any, tokens: any) => {
   expect(getShadowTokens(mode)).toBe(tokens);
