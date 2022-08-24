c5f3da1f8848dd6da96e30bc0bf8777270993771
nabil-k <duomaster01@gmail.com>
2022-08-24 13:56:45 -0700

    added test for createColorAliasTokens

--- packages/theming/default-theme/src/__tests__/default-theme.test.ts
+++ packages/theming/default-theme/src/__tests__/default-theme.test.ts
@@ -6,10 +6,12 @@ import darkShadowTokens from '@fluentui-react-native/design-tokens-windows/dark/
 import hcShadowTokens from '@fluentui-react-native/design-tokens-win32/hc/tokens-shadow.json';
 import { getAliasTokens } from '../getTokens';
 import { getShadowTokens } from '../getTokens';
+import { createColorAliasTokens } from '../createAliasTokens';
 import { createDefaultTheme } from '../createDefaultTheme';
 import { defaultFluentDarkTheme, defaultFluentHighConstrastTheme, defaultFluentTheme } from '../defaultTheme';
 import { ThemeReference } from '@fluentui-react-native/theme';
 import { Theme } from '@fluentui-react-native/theme-types';
+import { mapPipelineToShadow, mapPipelineToTheme } from '@fluentui-react-native/theming-utils';
 
 const lightThemeRef = new ThemeReference({} as Theme, defaultFluentTheme);
 const darkThemeRef = new ThemeReference({} as Theme, defaultFluentDarkTheme);
@@ -33,18 +35,24 @@ const getShadowTokensTable = [
   ['highContrast', hcShadowTokens.shadow],
 ];
 
-/**
- * Fails due to [Function anonymous] existing in the recipes key of what createDefaultTheme(themeOption) returns.
- * The expect does not expect this to exist [Function anonymous].
- */
-it.concurrent.each(createDefaultThemeTable)('createDefaultTheme test', async (themeOption: any, theme: any) => {
-  expect(createDefaultTheme(themeOption)).toBe(theme);
-});
+describe('default-theme tests', () => {
+  /**
+   * Fails due to [Function anonymous] existing in the recipes key of what createDefaultTheme(themeOption) returns.
+   * The expect does not expect this to exist [Function anonymous].
+   */
+  it.concurrent.each(createDefaultThemeTable)('createDefaultTheme test', async (themeOption: any, theme: any) => {
+    expect(createDefaultTheme(themeOption)).toBe(theme);
+  });
 
-it.concurrent.each(getAliasTokensTable)('getAliasTokens test', async (mode: any, tokens: any) => {
-  expect(getAliasTokens(mode)).toBe(tokens);
-});
+  it.concurrent.each(getAliasTokensTable)('getAliasTokens test', async (mode: any, tokens: any) => {
+    expect(getAliasTokens(mode)).toBe(tokens);
+  });
 
-it.concurrent.each(getShadowTokensTable)('getShadowTokens test', async (mode: any, tokens: any) => {
-  expect(getShadowTokens(mode)).toBe(tokens);
+  it.concurrent.each(getShadowTokensTable)('getShadowTokens test', async (mode: any, tokens: any) => {
+    expect(getShadowTokens(mode)).toBe(tokens);
+  });
+
+  it.concurrent.each(getAliasTokensTable)('createColorAliasTokens test', async (mode: any, tokens: any) => {
+    expect(createColorAliasTokens(mode)).toStrictEqual(mapPipelineToTheme(tokens));
+  });
 });
