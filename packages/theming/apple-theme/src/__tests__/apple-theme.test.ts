4275f38288666c1e53b448d6332f0834f0e7909b
nabil-k <duomaster01@gmail.com>
2022-08-24 17:23:14 -0700

    added createMacOSShadowAliasTokens test

--- packages/theming/apple-theme/src/__tests__/apple-theme.test.ts
+++ packages/theming/apple-theme/src/__tests__/apple-theme.test.ts
@@ -1,7 +1,8 @@
 import { createAppleTheme } from '../createAppleTheme';
 import { ThemeReference } from '@fluentui-react-native/theme';
 import { createDefaultTheme } from '@fluentui-react-native/default-theme';
-import { createMacOSColorAliasTokens } from '../createMacOSAliasTokens';
+import { createMacOSColorAliasTokens, createMacOSShadowAliasTokens } from '../createMacOSAliasTokens';
+import { mapPipelineToTheme, mapPipelineToShadow } from '@fluentui-react-native/theming-utils';
 import { getMacOSAliasTokens, getMacOSShadowTokens } from '../getMacOSTokens';
 import macOSLightAliasTokens from '@fluentui-react-native/design-tokens-macos/light/tokens-aliases.json';
 import macOSDarkAliasTokens from '@fluentui-react-native/design-tokens-macos/dark/tokens-aliases.json';
@@ -13,7 +14,7 @@ import macOSLightHCShadowTokens from '@fluentui-react-native/design-tokens-macos
 import macOSDarkHCShadowTokens from '@fluentui-react-native/design-tokens-macos/hcdark/tokens-shadow.json';
 import { getIsHighContrast, setIsHighContrast } from '../appleHighContrast.macos';
 
-const getMacOsAliasTokensTable = [
+const macOsAliasTokensTable = [
   ['light', true, macOSLightHCAliasTokens],
   ['light', false, macOSLightAliasTokens],
   ['dark', true, macOSDarkHCAliasTokens],
@@ -21,7 +22,7 @@ const getMacOsAliasTokensTable = [
   ['highContrast', null, null],
 ];
 
-const getMacOsAliasShadowTokensTable = [
+const macOsAliasShadowTokensTable = [
   ['light', true, macOSLightHCShadowTokens.shadow],
   ['light', false, macOSLightShadowTokens.shadow],
   ['dark', true, macOSDarkHCShadowTokens.shadow],
@@ -37,7 +38,7 @@ describe('apple-theme tests', () => {
   //   console.log(createDefaultTheme());
   // });
 
-  it.concurrent.each(getMacOsAliasTokensTable)('getMacOSAliasTokens test', async (mode: any, isHighContrast: any, tokens: any) => {
+  it.concurrent.each(macOsAliasTokensTable)('getMacOSAliasTokens test', async (mode: any, isHighContrast: any, tokens: any) => {
     if (mode !== 'highContrast') {
       expect(getMacOSAliasTokens(mode, isHighContrast)).toBe(tokens);
     } else {
@@ -45,7 +46,7 @@ describe('apple-theme tests', () => {
     }
   });
 
-  it.concurrent.each(getMacOsAliasShadowTokensTable)('getMacOSShadowTokens test', async (mode: any, isHighContrast: any, tokens: any) => {
+  it.concurrent.each(macOsAliasShadowTokensTable)('getMacOSShadowTokens test', async (mode: any, isHighContrast: any, tokens: any) => {
     if (mode !== 'highContrast') {
       expect(getMacOSShadowTokens(mode, isHighContrast)).toBe(tokens);
     } else {
@@ -59,4 +60,23 @@ describe('apple-theme tests', () => {
     setIsHighContrast(true);
     expect(getIsHighContrast()).toBe(true);
   });
+
+  it.concurrent.each(macOsAliasTokensTable)('createMacOSColorAliasTokens test', async (mode: any, isHighContrast: any, tokens: any) => {
+    if (mode !== 'highContrast') {
+      expect(createMacOSColorAliasTokens(mode, isHighContrast)).toEqual(mapPipelineToTheme(tokens));
+    } else {
+      expect(() => createMacOSColorAliasTokens(mode, isHighContrast)).toThrow();
+    }
+  });
+
+  it.concurrent.each(macOsAliasShadowTokensTable)(
+    'createMacOSShadowAliasTokens test',
+    async (mode: any, isHighContrast: any, tokens: any) => {
+      if (mode !== 'highContrast') {
+        expect(createMacOSShadowAliasTokens(mode, isHighContrast)).toEqual(mapPipelineToTheme(tokens));
+      } else {
+        expect(() => createMacOSShadowAliasTokens(mode, isHighContrast)).toThrow();
+      }
+    },
+  );
 });
